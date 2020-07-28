627e70686049080d25ab1a408435f10547b938fb
Vladislav Timakov <jrmnnl@gmail.com>
2020-07-28 10:10:24 +0300

    Init phoenix project

---
+++ lib/awesome_elixir/application.ex
@@ -0,0 +1,34 @@
+defmodule AwesomeElixir.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    children = [
+      # Start the Ecto repository
+      AwesomeElixir.Repo,
+      # Start the Telemetry supervisor
+      AwesomeElixirWeb.Telemetry,
+      # Start the PubSub system
+      {Phoenix.PubSub, name: AwesomeElixir.PubSub},
+      # Start the Endpoint (http/https)
+      AwesomeElixirWeb.Endpoint
+      # Start a worker by calling: AwesomeElixir.Worker.start_link(arg)
+      # {AwesomeElixir.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: AwesomeElixir.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  def config_change(changed, _new, removed) do
+    AwesomeElixirWeb.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
